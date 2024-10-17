9882b3ca6d88bcbbf7e5d54fd7639ea69d19338d
Liel Onairam Tan <liel.onairam.tan@gmail.com>
2024-10-17 16:19:25 +0800

    Add endpoints and routes for user status

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const {
   createUser,
-  getUsers,
-  getUser,
-  updateUser,
+  createUserStatus,
   deleteUser,
+  deleteUserStatus,
+  getUser,
+  getUserStatus,
+  getUsers,
+  getUsersStatus,
+  getEmployeeLocations,
+  updateUser,
+  updateUserStatus,
 } = require('../controllers/usersController');
 const { authorizeAnyRoles, requireAuthentication } = require('../middleware/auth');
 
@@ -15,6 +21,13 @@ router.use(requireAuthentication);
 
 router.post('/', authorizeAnyRoles('superuser'), createUser);
 router.get('/', authorizeAnyRoles('superuser', 'admin'), getUsers);
+
+router.post('/status', authorizeAnyRoles('superuser'), createUserStatus);
+router.get('/status', authorizeAnyRoles('superuser', 'admin'), getUsersStatus);
+router.get('/status/:id', getUserStatus);
+router.put('/status/:id', updateUserStatus);
+router.delete('/status/:id', authorizeAnyRoles('superuser'), deleteUserStatus);
+
 router.get('/:id', getUser);
 router.put('/:id', updateUser);
 router.delete('/:id', authorizeAnyRoles('superuser'), deleteUser);
