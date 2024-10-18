166fb6e23a2f9bc24ecaa5026555eee0a4ee9f47
Liel Onairam Tan <liel.onairam.tan@gmail.com>
2024-10-18 14:43:08 +0800

    Add code for populating initial database records

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const calamitiesRouter = require('./routes/calamities');
 const locationGroupsRouter = require('./routes/locationGroups');
 const usersRouter = require('./routes/users');
 const userStatusRouter = require('./routes/userStatus');
+const User = require('./models/userModel');
+const LocationGroup = require('./models/locationGroupModel');
 
 
 const app = express();
@@ -33,14 +35,77 @@ apiRouter.use('/users/status', userStatusRouter);
 apiRouter.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
-const onStartup = async () => {
-  console.log('On startup.');
-}
-
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
+  .then(async () => {
     console.log('Successfully connected to DB.');
+
+    // Mock data for Location Groups.
+    const municipalities = [
+      {
+        region: 'NCR',
+        province: 'Metro Manila',
+        municipality: 'Quezon City',
+      },
+      {
+        region: 'NCR',
+        province: 'Metro Manila',
+        municipality: 'City of Manila',
+      },
+      {
+        region: 'NCR',
+        province: 'Metro Manila',
+        municipality: 'Taguig City',
+      },
+      {
+        region: 'NCR',
+        province: 'Metro Manila',
+        municipality: 'Pasig City',
+      },
+      {
+        region: 'NCR',
+        province: 'Metro Manila',
+        municipality: 'Makati City',
+      },
+    ];
+
+    for (const item of municipalities) {
+      const { region, province, municipality } = item;
+      const code = `PH_${region}_${province.replace(' ', '')}_${municipality.replace(' ', '')}`;
+
+      let exist = await LocationGroup.findOne({code}).lean();
+      if (exist) {
+        continue;
+      }
+
+      await LocationGroup.create({
+        code,
+        country: 'Philippines',
+        region,
+        province,
+        municipality,
+      });
+    }
+
+    // Create superuser.
+    let exist = await User
+      .findOne({email: 'owner@codev.com'})
+      .lean();
+
+    if (!exist) {
+      const userLG = await LocationGroup
+        .findOne({municipality: 'Quezon City'})
+        .lean();
+        
+      await User.validateThenCreate({
+        email: 'owner@codev.com',
+        password: 'Password123!',
+        roles: ['SUPERUSER'],
+        firstName: 'Owner',
+        lastName: 'CoDev',
+        locationGroup: userLG,
+      });
+    }
   })
   .catch((error) => {
     console.log(error);
